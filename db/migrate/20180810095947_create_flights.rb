0e50c85610c7190ef0a890f7176603b1f1ac8b9e
Satan_petit-coeur <plantevin.contact@gmail.com>
2018-08-10 14:53:09 +0200

    init

---
+++ db/migrate/20180810095947_create_flights.rb
@@ -0,0 +1,11 @@
+class CreateFlights < ActiveRecord::Migration[5.2]
+  def change
+    create_table :flights do |t|
+      t.integer :duration
+      t.datetime :departure_date
+      t.string :departure_airport
+      t.string :arrival_airport
+      t.timestamps
+    end
+  end
+end
